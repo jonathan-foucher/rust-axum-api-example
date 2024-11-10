aa9d2dc935e470a4328d2f0bf7f4a9d2992276c5
Jonathan Foucher <jonathan-foucher@outlook.fr>
2024-11-10 14:30:11 +0100

    use env var to define http port + add logger

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use axum::{routing::get, Router};
+use std::env;
+
+const DEFAULT_HTTP_PORT: u16 = 8080;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let http_port: u16 = env::var("HTTP_PORT")
+        .map(|http_port| http_port.parse::<u16>().unwrap_or(DEFAULT_HTTP_PORT))
+        .unwrap_or(DEFAULT_HTTP_PORT);
+
     let app = Router::new()
         .route("/", get(root));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    log::info!("Application is starting on port {}", http_port);
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", http_port)).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
